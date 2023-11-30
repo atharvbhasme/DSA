6998f0a1f362f8123c61f443c34fdfa87d417558
Atharv Bhasme <atharv.bhasme@gmail.com>
2023-12-01 00:05:37 +0530

    day 3

---
+++ number_system/countTheNumber.java
@@ -0,0 +1,23 @@
+package number_system;
+
+import java.util.Scanner;
+
+public class countTheNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        int copy = n;
+        while(n>0){
+            int digit = n%10;
+            if(digit == 0){
+                n = n/10;
+                continue;
+            }
+            if(copy % digit == 0){
+                count ++;
+            }
+            n = n/10;
+        }
+    }
+}
