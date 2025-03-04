6c1030ee065888a6834cf4a91a0396610dfe5652
Atharv Bhasme <atharvbhasme@gmail.com>
2025-03-04 23:27:34 +0530

    make it without collections

--- Arrays_medium/code9.java
+++ Arrays_medium/code9.java
@@ -1,55 +1,55 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class code9 {
     public static void main(String[] args) {
-        List<Integer> A = Arrays.asList(new Integer[] {2, 1, 5, 4, 3, 0, 0});
-        List<Integer> ans = nextGreaterPermutation(A);
-
-        System.out.print("The next permutation is: [");
-        for (int i = 0; i < ans.size(); i++) {
-            System.out.print(ans.get(i) + " ");
-        }
-        System.out.println("]");
+        int[] arr = {1,3,2};
+        nextPermutation(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static List< Integer > nextGreaterPermutation(List< Integer > A) {
-        int n = A.size(); // size of the array.
+    public static void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int ind = -1;
 
-        // Step 1: Find the break point:
-        int ind = -1; // break point
+        // Step 1: Find the breakpoint where nums[i] < nums[i+1]
         for (int i = n - 2; i >= 0; i--) {
-            if (A.get(i) < A.get(i + 1)) {
-                // index i is the break point
+            if (nums[i] < nums[i + 1]) {
                 ind = i;
                 break;
             }
         }
 
-        // If break point does not exist:
+        // If no breakpoint found, reverse the whole array (last permutation case)
         if (ind == -1) {
-            // reverse the whole array:
-            Collections.reverse(A);
-            return A;
+            reverse(nums, 0, n - 1);
+            return;
         }
 
-        // Step 2: Find the next greater element
-        //         and swap it with arr[ind]:
-
-        for (int i = n - 1; i > ind; i--) {
-            if (A.get(i) > A.get(ind)) {
-                int tmp = A.get(i);
-                A.set(i, A.get(ind));
-                A.set(ind, tmp);
+        // Step 2: Find the next larger element to swap with nums[ind]
+        for (int i = n - 1; i > ind; i--) {  // Fix: Iterate till `n-1`
+            if (nums[i] > nums[ind]) {
+                swap(nums, i, ind);
                 break;
             }
         }
 
-        // Step 3: reverse the right half:
-        List<Integer> sublist = A.subList(ind + 1, n);
-        Collections.reverse(sublist);
+        // Step 3: Reverse the portion after ind (to get the next lexicographic permutation)
+        reverse(nums, ind + 1, n - 1);
+    }
 
-        return A;
+    // Utility function to reverse part of the array in-place
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    // Utility function to swap two elements
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
