a578735df2cb9143c3f086be30ac9e5ca0288ed6
Atharv Bhasme <atharvbhasme@gmail.com>
2025-03-26 23:24:46 +0530

    Added java methods

---
+++ java_basic/Methods.java
@@ -0,0 +1,32 @@
+package java_basic;
+
+public class Methods {
+    public static void main(String[] args) {
+        Demo dm = new Demo();
+        Demo.staticMethod();
+    }
+}
+
+class Demo {
+   //creating constructor
+    static int a;
+    String dr = "dr";
+   Demo(){
+       System.out.println("Calling constructor");
+       a = 0;
+   }
+
+   public static void staticMethod (){
+       System.out.println("This is an Static method");
+       System.out.println(a);
+       Demo d = new Demo();
+       d.publicMethod();
+   }
+
+   public void publicMethod(){
+       System.out.println("This is public method");
+       System.out.println("This is String " + dr);
+       System.out.println("This is int "+ a);
+   }
+
+}
